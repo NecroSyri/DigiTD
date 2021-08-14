49820baf2c229eb7d9f40eb624a54565171f3557
Syrius <syrius.belmont@gmail.com>
2021-08-14 10:23:41 +0200

    remove elements and update

--- js/elements.js
+++
@@ -1,3 +0,0 @@
-export let elements = {
-    container : document.querySelector(".container")
-}
--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,29 @@
-import { elements } from "./elements";
-
 export let menu = {
-    // FUNCTIONS
-    createScreen : function(){
-        // Create elements
-        let btnPlay = document.createElement("button");
-        let btnOptions = document.createElement("button");
-        
-        // Add classes
-        btnPlay.classList.add("play");
-        btnOptions.classList.add("options");
-        
-        // Append
-        elements.container.append(btnPlay);
-        elements.container.append(btnOptions);
-        
-        // Add content
-        btnPlay.innerHTML = "Play";
-        btnOptions.innerHTML = "Options";
-    },
-    
     // INIT
     init : function(){
-        var elements = import("./elements");
-        menu.createScreen();
+        // ELEMENTS
+        let container = document.querySelector("#container");
+
+        // FUNCTIONS
+        function createScreen(){
+            // Create elements
+            let btnPlay = document.createElement("button");
+            let btnOptions = document.createElement("button");
+            
+            // Add classes
+            btnPlay.classList.add("play");
+            btnOptions.classList.add("options");
+            
+            // Append
+            container.append(btnPlay);
+            container.append(btnOptions);
+            
+            // Add content
+            btnPlay.innerHTML = "Play";
+            btnOptions.innerHTML = "Options";
+        }
+
+        // MAIN
+        createScreen();
     }
 }
